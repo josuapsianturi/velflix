66dfa4b0ba823ee9710fc047613cf97b4066423f
josuapsianturi <josuasianturi561@gmail.com>
2021-04-15 23:17:00 +0700

    show movies - add description

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -16,7 +16,7 @@
     <div class="container mx-auto overflow-y-auto rounded-lg lg:px-56">
         <div class="bg-black rounded-xl">
                 <div class="relative overflow-hidden responsive-container" style="padding-top: 56.25%">
-                    <iframe class="absolute top-0 left-0 w-full h-full rounded-xl responsive-iframe"
+                    <iframe class="absolute top-0 left-0 w-full h-full rounded-t-xl responsive-iframe"
                         src="https://www.youtube.com/embed/{{ $movies['videos']['results'][0]['key'] }}"
                         style="border:0;" allow="autoplay; encrypted-media" allowfullscreen>
                     </iframe>
@@ -24,7 +24,7 @@
 
             <div class="px-8 py-3 modal-body">
                 <div class="relative overflow-hidden text-white responsive-container">
-                    <div class="flex flex-row w-full m-4 space-x-4">
+                    <div class="flex flex-row w-full my-4 space-x-4">
                         <button class="flex items-center justify-center px-2 py-2 space-x-2 bg-white rounded shadow-md bg-gradient w-28 ">
                             <x-bi-caret-right-fill class="w-6 h-6 text-black" />
                             <span class="font-semibold text-black">Play</span>
@@ -40,15 +40,26 @@
                         </button>
                     </div>
 
-                    <div class="flex my-3 space-x-4">
-                        <span class="font-bold text-green-500">{{ $movies['vote_average'] * 10 . '%' }} Match</span>
-                        <span>{{ date('Y', strtotime($movies['release_date'] )) }}</span>
-                    </div>
+                    <div class="flex my-4">
+                        <div class="w-4/6">
+                            <span class="flex mb-3 space-x-4">
+                                <p class="font-bold text-green-500">{{ $movies['vote_average'] * 10 . '%' }} Match</p>
+                                <p>{{ date('Y', strtotime($movies['release_date'] )) }}</p>
+                            </span>
 
-                    <div class="">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Labore omnis qui doloribus cupiditate tenetur accusamus earum placeat minus error sint enim,
-                    sequi exercitationem, dolorem harum commodi voluptates. Sequi, deserunt minus.
+                            <span>
+                                {{ $movies['overview'] }}
+                            </span>
+                        </div>
+
+                        <div class="w-2/6">
+                            <span class="flex mb-3 text-gray-500">
+                                Cast:
+                            </span>
+                            <span class="text-gray-500">
+                                Genres:
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
