b28958d3c4606c69b771458db0b3bb7247048ed1
Josua Putra Sianturi <josuapsianturi@gmail.com>
2022-05-25 16:14:54 +0700

    Refactoring routes to route groups

--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,26 @@ use App\Http\Livewire\AdminController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/movies', [VelflixController::class, 'index'])->name('velflix.index')->middleware('auth');
-Route::get('/movie/{watch}', [VelflixController::class, 'show'])->name('movies.show')->middleware('auth');
 Route::view('/', 'home');
-
 Route::post('newsletter', NewsletterController::class);
 
-Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
-Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+Route::middleware('guest')->group(function () {
+    Route::get('login', [SessionsController::class, 'create'])->name('login');
+    Route::post('login', [SessionsController::class, 'store']);
+    Route::get('register', [RegisterController::class, 'create'])->name('register');
+    Route::post('register', [RegisterController::class, 'store']);
+});
 
-Route::get('login', [SessionsController::class, 'create'])->name('login')->middleware('guest');
-Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
-Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [SessionsController::class, 'destroy'])->name('logout');
+    Route::get('/movies', [VelflixController::class, 'index'])->name('velflix.index');
+    Route::get('/movie/{watch}', [VelflixController::class, 'show'])->name('movies.show');
+});
 
-Route::get('login/google', [LoginController::class, 'redirectToProvider']);
-Route::get('login/google/callback', [LoginController::class, 'handleProviderCallback']);
+Route::controller(LoginController::class)->group( function () {
+    Route::get('login/google', 'redirectToProvider');
+    Route::get('login/google/callback', 'handleProviderCallback');
+});
 
 Route::middleware('can:admin')->group(function () {
     Route::view('admin', 'livewire.admin-controller');
