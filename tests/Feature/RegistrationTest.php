4857d5520831d6c3745d1138548911a060fb4dcf
Josua Putra Sianturi <josuapsianturi@gmail.com>
2022-05-27 02:11:44 +0700

    Test - add test new users can register

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -1,4 +1,21 @@
 <?php
 
-it('test_registration_screen_can_be_rendered')->get('/register')->assertStatus(200);
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+test('test registration screen can berendered')->get('/register')->assertStatus(200);
+
+test('test new users can register', function ()
+{
+    $response = $this->post('/register', [
+        'name' => 'User Test',
+        'username' => 'userTest',
+        'email' => 'test@example.com',
+        'password' => 'testpassword',
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect('/movies');
+});
 
