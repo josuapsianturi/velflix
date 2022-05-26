6f347c18ce867f4014a9ff9f20793ef68ce0f89e
Josua Putra Sianturi <josuapsianturi@gmail.com>
2022-05-27 02:33:28 +0700

    Test - Add Test users can authenticate using the login screen

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -1,3 +1,19 @@
 <?php
 
+// use App\Models\User;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+// uses(RefreshDatabase::class);
+
 test('Test login screen can be rendered')->get('/login')->assertStatus(200);
+
+test('Test users can authenticate using the login screen', function ()
+{
+    $response = $this->post('/login', [
+        'email' => 'user@gmail.com',
+        'password' => 'password',
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect('/movies');
+});
