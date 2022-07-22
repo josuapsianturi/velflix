acd920dd9e10fe644c86b6d662270b9708291c2c
Josua Putra Sianturi <josuapsianturi@gmail.com>
2022-07-22 14:54:51 +0700

    Cypresss - add valid login test

--- tests/cypress/integration/auth_spec.cy.js
+++ tests/cypress/integration/auth_spec.cy.js
@@ -1,9 +1,20 @@
 describe('Authentication Test', () => {
-    it('Provides feedback for invalid login credentials', () => {
+    beforeEach(() => {
+        cy.refreshDatabase().seed();
+    });
+
+    it('Provides feedback for invalid email', () => {
+        cy.visit('/login');
+        cy.get('#email').type('asdf@gmail.com');
+        cy.get('#password').type('password');
+        cy.contains('button', 'Log In').click();
+        cy.contains('Your provieded email could not be verified.');
+    });
+
+    it('Valid login', () => {
         cy.visit('/login');
         cy.get('#email').type('user@gmail.com');
         cy.get('#password').type('password');
         cy.contains('button', 'Log In').click();
-        cy.contains('Your provieded credentials could not be verified.');
     });
 });
