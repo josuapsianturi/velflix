2dfa532fa09cb73c2bae56ff7104547ab2955c6d
Josua P. Sianturi <josuapsianturi@gmail.com>
2023-03-04 21:29:06 +0700

    remove sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -41,13 +41,4 @@ class Handler extends ExceptionHandler
             //
         });
     }
-
-    public function report(Throwable $exception)
-    {
-        if (app()->bound('sentry') && $this->shouldReport($exception)) {
-            app('sentry')->captureException($exception);
-        }
-
-        parent::report($exception);
-    }
 }
