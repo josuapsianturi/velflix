eaf93c5d427665a805b9e8184996b0f563b31c92
Nifleheim <reza.fabryan@gmail.com>
2023-04-11 12:26:20 +0700

    Helm Deployment

--- resources/views/components/gap.blade.php
+++
@@ -1,1 +0,0 @@
-<div class="h-3 w-full" style="background-color: #222"></div>
--- resources/views/main.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout>
-    <x-header />
-
-    <x-nav :popular='$popular' />
-
-    <x-movies.index  :popular='$popular' :genres='$genres' :trending='$trending' :comedies='$comedies' :action='$action' :western='$western' :horror='$horror' :thriller='$thriller' :animation='$animation' />
-
-    <x-footer />
-</x-layout>
